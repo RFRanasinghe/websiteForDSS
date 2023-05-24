2fd6f9c9bc04134fe18361ce58d54d289ea05d3a
Ruchira Ranasinghe <91824033+RFRanasinghe@users.noreply.github.com>
2023-05-24 08:46:50 +0530

    Delete src directory

--- src/app/mobile-page/mobile-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-mobile-page',
-  templateUrl: './mobile-page.component.html',
-  styleUrls: ['./mobile-page.component.css']
-})
-export class MobilePageComponent {
-}
